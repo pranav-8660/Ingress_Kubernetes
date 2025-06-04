9a5d3a2af607102753715eb25aae888952477ef4
pranav-8660 <pranavcse2003@gmail.com>
2025-06-04 16:26:22 +0530

    configures the delete (apply and delete) files

---
+++ kubernetes/scriptdel.sh
@@ -0,0 +1,5 @@
+kubectl delete -f deployment-app-a.yaml
+kubectl delete -f service-app-a.yaml
+kubectl delete -f deployment-app-b.yaml
+kubectl delete -f service-app-b.yaml
+kubectl delete -f ingress.yml 
--- kubernetes/scriptrun.sh
+++ kubernetes/scriptrun.sh
@@ -2,4 +2,4 @@ kubectl apply -f deployment-app-a.yaml
 kubectl apply -f service-app-a.yaml
 kubectl apply -f deployment-app-b.yaml
 kubectl apply -f service-app-b.yaml
-kubectl apply -f ingress.yaml
+kubectl apply -f ingress.yml
