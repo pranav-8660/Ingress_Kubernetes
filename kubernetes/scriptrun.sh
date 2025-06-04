7ebd27d91b9365c2c0b5f0b1084a4f3002e194df
pranav-8660 <pranavcse2003@gmail.com>
2025-06-04 16:09:00 +0530

    added the ingress.yml

---
+++ kubernetes/scriptrun.sh
@@ -0,0 +1,5 @@
+kubectl apply -f deployment-app-a.yaml
+kubectl apply -f service-app-a.yaml
+kubectl apply -f deployment-app-b.yaml
+kubectl apply -f service-app-b.yaml
+kubectl apply -f ingress.yaml
